fbeed860a724d37a07d4a1ea37fa6ebc80b4a2f4
Ayush yadav <20ayush07yadav03@gmail.com>
2022-05-02 10:37:58 +0530

    second-commit

--- index.js
+++ index.js
@@ -219,8 +219,24 @@ canvas.on('mouse:wheel', function(opt) {
     var zoom = canvas.getZoom();
     zoom *= 0.999 ** delta;
     if (zoom > 20) zoom = 20;
-    if (zoom < 0.01) zoom = 0.01;
+    if (zoom < 1) zoom = 1;
     canvas.zoomToPoint({ x: opt.e.offsetX, y: opt.e.offsetY }, zoom);
     opt.e.preventDefault();
     opt.e.stopPropagation();
-  });
+    var vpt = this.viewportTransform;
+    if (zoom < 400 / 1000) {
+      vpt[4] = 200 - 1000 * zoom / 2;
+      vpt[5] = 200 - 1000 * zoom / 2;
+    } else {
+      if (vpt[4] >= 0) {
+        vpt[4] = 0;
+      } else if (vpt[4] < canvas.getWidth() - 1000 * zoom) {
+        vpt[4] = canvas.getWidth() - 1000 * zoom;
+      }
+      if (vpt[5] >= 0) {
+        vpt[5] = 0;
+      } else if (vpt[5] < canvas.getHeight() - 1000 * zoom) {
+        vpt[5] = canvas.getHeight() - 1000 * zoom;
+      }
+    }
+    });
